f62f3e10ad8a2ccce70807fce9d92ecb33e38cea
dg2894 <dg2894@g.rit.edu>
2017-02-28 18:59:59 -0500

    Initial commit

---
+++ Project-Collabyrinth/Assets/Collabyrinth/Scripts/ObjectInteraction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ObjectInteraction : MonoBehaviour {
+
+    // Use this for initialization
+    public Transform controllerPos;
+    
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void PickUpObject()
+    {
+      transform.parent = controllerPos;
+    }
+
+    public void PutDownObject()
+    {
+        transform.parent = null;
+    }
+}
